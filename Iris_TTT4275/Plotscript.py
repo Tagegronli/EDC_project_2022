450033c508da71321bdf45b527a47cc2d7e8dfde
Tage Grønli <tgo@shifthyperloop.com>
2022-03-29 14:24:09 +0200

    fixing

--- Iris_TTT4275/Plotscript.py
+++ Iris_TTT4275/Plotscript.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
@@ -11,21 +10,19 @@ def plot_objects(filename, color, name, plt1, plt2):
         class_matrix.append(obj)
 
 
-
     #plotting points with labels (so that we can display the label of 1 point)
-    plt1.plot(float(class_matrix[0][0]), float(class_matrix[0][1]), marker="o", color=color, label = name)
+    plt1.plot(float(class_matrix[0][0]), float(class_matrix[0][1]), marker="o", color=color, label=name)
     plt1.legend() #Showing only label of 1 point so we dont get 1000 labels
 
-    plt2.plot(float(class_matrix[0][2]), float(class_matrix[0][3]), marker="o", color=color, label = name)
+    plt2.plot(float(class_matrix[0][2]), float(class_matrix[0][3]), marker="o", color=color, label=name)
     plt2.legend() #Showing only label of 1 point so we dont get 1000 labels
 
 
 
     #plotting the rest of the points without labels
     for obj in class_matrix[1:]:
-        plt1.plot(float(obj[0]), float(obj[1]), marker = "o", color= color) #, label = "AAA")
+        plt1.plot(float(obj[0]), float(obj[1]), marker="o", color=color) #, label = "AAA")
         plt2.plot(float(obj[2]), float(obj[3]), marker="o", color=color) #, label = "AAA")
-
 
 
     return
@@ -55,6 +52,9 @@ def plottscript():
     plot_objects("class_2.csv", "green", "Versicolor", ax1, ax2)
     plot_objects("class_3.csv", "blue", "Virginica", ax1, ax2)
     plt.show()
+
     return
+
+
 
 plottscript()
