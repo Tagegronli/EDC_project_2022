55d77ff423a4a0e5617d15a679885849d16a4d91
Elias Prytz <asdero63@gmail.com>
2022-04-06 19:22:38 +0200

    Works with and without clustering

--- MNist_ttt4275/classifier/classify.py
+++ MNist_ttt4275/classifier/classify.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import time
 from classifier import dataaccess
+from sklearn.cluster import KMeans
 
 def get_nearest_neighbours(templates, template_labels, k, image, maxcheck=10000):
     npimage = np.array(image)
@@ -30,6 +31,7 @@ def print_guess(guess, answer, image=None):
     print()
 
 def classify_images(templates, template_labels, images, images_labels, print_guesses=False):
+    t_init = time.time()
     K = 11
     NUMCLASSES = 10
     confusion_matrix = [[0]*NUMCLASSES for i in range(NUMCLASSES)]
@@ -37,7 +39,7 @@ def classify_images(templates, template_labels, images, images_labels, print_gue
     displayederrors = 0
     displayedcorrect = 0
     for index in range(len(images)):
-        guess = make_guess(templates, template_labels, K, images[index], maxcheck=1000)
+        guess = make_guess(templates, template_labels, K, images[index], maxcheck=100)
         fasit = images_labels[index]
         confusion_matrix[fasit][guess] += 1
         if fasit == guess:
@@ -53,4 +55,45 @@ def classify_images(templates, template_labels, images, images_labels, print_gue
         if print_guesses:
             print_guess(guess, fasit)
             print()
-    return (correct_guesses/len(images), confusion_matrix)
+    return (correct_guesses/len(images), confusion_matrix, int((time.time()-t_init)*1000))
+
+def classify_with_clusters(testing_images, testing_labels):
+    NUMCLASSES = 10
+
+    training_data = dataaccess.get_training_data(by_class=True)
+    templates = create_clustered_templates(training_data)
+
+    # classify testing_images
+    t_init = time.time() # timing just the classifying
+    corrects = 0
+    confusion_matrix = [[0]*NUMCLASSES for i in range(NUMCLASSES)]
+    for idx, image in enumerate(testing_images):
+        nearestneighbors = get_nearest_neighbours_clustered(templates, 7, image, cluster_per_class=64)
+        guess = np.bincount(nearestneighbors).argmax()
+        answer = testing_labels[idx]
+        confusion_matrix[answer][guess] += 1
+        if guess == answer:
+            corrects += 1
+
+    return (1-(corrects/len(testing_images)), np.matrix(confusion_matrix), int((time.time()-t_init)*1000))
+
+def create_clustered_templates(training_data):
+    CLUSTERS_PER_CLASS = 64
+    NUMCLASSES = 10
+    templates = [list() for i in range(NUMCLASSES)]
+    for idx, cls in enumerate(training_data):
+        model = KMeans(n_clusters=CLUSTERS_PER_CLASS)
+        model.fit(cls)
+        templates[idx] = model.cluster_centers_
+        print("Calculated clusters for %d" % idx)
+    return templates
+
+
+def get_nearest_neighbours_clustered(templates, k, image, cluster_per_class=64):
+    npimage = np.array(image)
+    distances = list()
+    for cls_templates in templates:
+        distances.extend([np.linalg.norm(npimage-np.array(temp)) for temp in cls_templates])    
+    partitioned = np.argpartition(distances, k)
+    idx_k_smallest = partitioned[:k]
+    return np.array([floor(index/cluster_per_class) for index in idx_k_smallest])
--- MNist_ttt4275/classifier/main.py
+++ MNist_ttt4275/classifier/main.py
@@ -2,9 +2,9 @@ import time
 import numpy as np
 from math import floor
 from classifier import dataaccess, cli, classify 
-
 from sklearn.cluster import KMeans
-from sklearn.datasets import make_classification
+from sklearn.metrics import confusion_matrix
+
 
 def run():
     
@@ -16,60 +16,23 @@ def run():
     
     training_images, training_labels = dataaccess.get_training_data(by_class=False)
     testing_images, testing_labels = dataaccess.get_testing_data()
-
-    CLASSES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     
     ######## WITHOUT CLUSTERING ################
-    # print("Classifying testing images with templates = training images ...")
-    # accuracy, confusion_matrix = classify.classify_images(training_images, training_labels, testing_images, testing_labels, print_guesses=True)
-    # print("Confusion matrix:\n", np.matrix(confusion_matrix))
-    # print("Error rate: %f" % (1-accuracy))
+    print("Classifying testing images with templates = training images ...")
+    accuracy, confusion_matrix, t = classify.classify_images(training_images, training_labels, testing_images, testing_labels, print_guesses=False)
+    print("Time used: %d ms" % t)
+    print("Error rate: %f" % (1-accuracy))
+    print("Confusion matrix:\n", np.matrix(confusion_matrix))
     #############################################
     
     ############## WITH CLUSTERING ##############
-    templates = [list() for i in range(10)]
-    training_data = dataaccess.get_training_data(by_class=True)
-    for idx, cls in enumerate(training_data):
-        model = KMeans(n_clusters=64)
-        #print(cls[0])
-        model.fit(cls)
-        templates[idx] = model.cluster_centers_
-        print("Calculated clusters for %d" % idx)
-        # print(model.cluster_centers_[63])
-    
-
-    corrects = 0
-    NUMCLASSES = 10
-    confusion_matrix = [[0]*NUMCLASSES for i in range(NUMCLASSES)]
-    for idx, image in enumerate(testing_images):
-        nearestneighbors = get_nearest_neighbours_clustered(templates, 7, image, cluster_per_class=64)
-        guess = np.bincount(nearestneighbors).argmax()
-        answer = testing_labels[idx]
-        confusion_matrix[answer][guess] += 1
-        classify.print_guess(guess, answer)
-        if guess == answer:
-            corrects += 1
-
-    # accuracy, confusion_matrix = classify.classify_images(training_images, training_labels, testing_images, testing_labels, print_guesses=True)
-    print("Error rate:", 1-(corrects/len(testing_images)))
-    print("Confusion matrix:")
-    print(np.matrix(confusion_matrix))
-
+    print("Classifying testing images with clustered templates ...")
+    error_rate, confusion_matrix, t = classify.classify_with_clusters(testing_images, testing_labels)
+    print("Time used: %d ms" % t)
+    print("Error rate:", error_rate)
+    print("Confusion matrix:\n", confusion_matrix)
     #############################################
 
     print("Total run time: %d ms" % ((time.time()-t_init)*1000))
     # sys.exit(1)
 
-def get_nearest_neighbours_clustered(templates, k, image, cluster_per_class=64):
-    npimage = np.array(image)
-    distances = list()
-    for cls_templates in templates:
-        distances.extend([np.linalg.norm(npimage-np.array(temp)) for temp in cls_templates])    
-    partitioned = np.argpartition(distances, k)
-    idx_k_smallest = partitioned[:k]
-    return np.array([floor(index/cluster_per_class) for index in idx_k_smallest])
-
-def make_guess(templates, template_labels, k, image, maxcheck=10000):
-    nearest_neighbors = get_nearest_neighbours_clustered(templates, template_labels, k, image, maxcheck=maxcheck)
-    guess = np.bincount(nearest_neighbors).argmax()
-    return guess
